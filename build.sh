c47878c99712c8c0d3cf4705db163b991c974a07
miguelDuqueUser <50363944+miguelDuqueUser@users.noreply.github.com>
2019-05-08 09:59:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 sudo apt install docker.io docker-compose -y
 sudo usermod -aG docker $USER
 sudo docker-compose up -d
+echo "###### INICIA EN NAVEGADOR WEB LA DIRECCIÓN: HTTPS://LOCALHOST:8080 #####"
