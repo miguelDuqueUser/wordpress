570644e3bed0c316eab38fb4962b84ef0d758ef6
miguelDuqueUser <50363944+miguelDuqueUser@users.noreply.github.com>
2019-05-15 16:55:28 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt install docker.io docker-compose -y
 sudo usermod -aG docker $USER
+cd /home/$USER/wordpress/
 sudo docker-compose up -d
 echo
 echo "###### INICIA EN NAVEGADOR WEB LA DIRECCIÓN: HTTPS://LOCALHOST:8080 #####"
